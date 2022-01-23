038011da3d91234b7bf99601862d278ec757d75a
JunHo Lee <kou2004@naver.com>
2022-01-23 14:59:27 +0900

    220123

---
+++ coding_test/Baekjoon_11718_그대로출력하기.py
@@ -0,0 +1,12 @@
+# 문제 : https://www.acmicpc.net/problem/11718
+
+while True:
+    try:
+        print(input())
+    except EOFError:
+        break
+
+# EOFError
+# EOF : End Of File
+# 텍스트 파일의 끝
+# 종료 조건이 명시 되어있지 않은 문제
--- coding_test/Baekjoon_2870_수학숙제.py
+++ coding_test/Baekjoon_2870_수학숙제.py
@@ -15,3 +15,23 @@ if __name__ == "__main__":
 
     for number in numbers:
         print(number)
+
+'''
+# 정규식 없이 풀기
+# https://www.acmicpc.net/source/37255315
+
+n = []
+for _ in range(int(input())):
+    t = ''
+    for i in input():
+        if i.isalpha() and t:
+            n.append(int(t))
+            t = ''
+        elif i.isdigit():
+            t += i
+    if t:
+        n.append(int(t))
+n.sort()
+for i in n:
+    print(i)
+'''
